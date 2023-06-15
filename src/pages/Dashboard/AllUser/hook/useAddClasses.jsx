679accb156dec9f8a5b6199efbbedf8345d39242
bossnoyon00 <bossnoyon584@gmail.com>
2023-06-16 00:29:39 +0600

    use Addm class hook

--- src/pages/Dashboard/AllUser/hook/useAddClasses.jsx
+++ src/pages/Dashboard/AllUser/hook/useAddClasses.jsx
@@ -2,13 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 
 const useAddClasses = () => {
 
-    const { data: addClasses = [], isLoading: loading, refetch } = useQuery({
-        queryKey: ['addClasses'],
-        queryFn: async () => {
-            const res = await fetch('https://summer-season.vercel.app/class');
-            return res.json();
-        }
-    })
+   
 
     return [addClasses, loading, refetch]
 }
