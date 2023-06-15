47335920a12edb7cc0865bca27e409b522561771
bossnoyon00 <bossnoyon584@gmail.com>
2023-06-16 00:24:41 +0600

    add class

--- src/pages/Home/Home/GoogleSignIn/GoogleSignIn.jsx
+++ src/pages/Home/Home/GoogleSignIn/GoogleSignIn.jsx
@@ -40,7 +40,9 @@ const GoogleSignIn = () => {
             })
     }
     return (
-       
+        <>
+            <button onClick={handleGoogleLogin} className="mx-auto mb-4 px-8 btn text-white ">Sign In With <FaGoogle className="text-primary "></FaGoogle> </button>
+        </>
     );
 };
 
