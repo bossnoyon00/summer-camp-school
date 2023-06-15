f918c54ac09206d803a6cec48570f5a36f20d2c6
bossnoyon00 <bossnoyon584@gmail.com>
2023-06-16 00:32:11 +0600

    payment issue solved

--- src/pages/Dashboard/AllUser/hook/usePayment.jsx
+++ src/pages/Dashboard/AllUser/hook/usePayment.jsx
@@ -3,7 +3,17 @@ import { useContext } from 'react';
 import { AuthContext } from '../../../Home/provider/AuthProvider';
 import useAxios from './useAxios';
 const usePayment = () => {
-   
+    const { user, loading } = useContext(AuthContext)
+    const [axiosSecure] = useAxios();
+    const { refetch, data: paymentHistory = [] } = useQuery({
+        queryKey: ['paymentHistory', user?.email],
+        enabled: !loading,
+        queryFn: async () => {
+            const res = await axiosSecure(`/studentsPaymentsHistory?email=${user?.email}`)
+            console.log('res from axios', res)
+            return res.data;
+        },
+    })
 
     return [paymentHistory,  refetch]
 
