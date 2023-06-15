9ac1d60e7ae8dafe72bc58ecc483b972274baf0f
bossnoyon00 <bossnoyon584@gmail.com>
2023-06-16 00:29:00 +0600

    add axios load

--- src/pages/Dashboard/AllUser/hook/useSelectClass.jsx
+++ src/pages/Dashboard/AllUser/hook/useSelectClass.jsx
@@ -5,7 +5,15 @@ import useAxios from './useAxios';
 const useSelectClass = () => {
     const { user, loading } = useContext(AuthContext);
     // const token = localStorage.getItem('access-token');
-   
+    const [axiosSecure] = useAxios()
+    const { refetch, data: cart = [] } = useQuery({
+        queryKey: ['carts', user?.email],
+        enabled: !loading,
+        queryFn: async () => {
+            const res = await axiosSecure(`/enroll?email=${user?.email}`)
+            console.log('res from axios', res)
+            return res.data;
+        },
     })
 
     return [cart, refetch]
