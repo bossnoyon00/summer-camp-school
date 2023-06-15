098bc4035c0c27e8e4b6d152e6a0742c17568a77
bossnoyon00 <bossnoyon584@gmail.com>
2023-06-16 00:34:00 +0600

    successfully enroll

--- src/pages/Dashboard/AllUser/hook/useSuccessEnroll.jsx
+++ src/pages/Dashboard/AllUser/hook/useSuccessEnroll.jsx
@@ -4,7 +4,19 @@ import { useContext } from 'react';
 import { AuthContext } from '../../../Home/provider/AuthProvider';
 
 const useSuccessEnroll = () => {
-    
+    const { user, loading } = useContext(AuthContext)
+    // const token = localStorage.getItem('access-token');
+    const [axiosSecure] = useAxios();
+    const { refetch, data: enrolledClass = [] } = useQuery({
+        queryKey: ['enrolledClass', user?.email],
+        enabled: !loading,
+
+        queryFn: async () => {
+            const res = await axiosSecure(`/enrolledClass?email=${user?.email}`)
+            // console.log('res from axios', res)
+            return res.data;
+        },
+    })
 
     return [enrolledClass, refetch]
 
