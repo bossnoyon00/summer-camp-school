dd5ae9fcf180a93fecdd33d80a1f1e05e7ebc952
bossnoyon00 <bossnoyon584@gmail.com>
2023-06-16 00:25:21 +0600

    added error element

--- src/pages/Home/Dashboard/AddClass/AddClass.jsx
+++ src/pages/Home/Dashboard/AddClass/AddClass.jsx
@@ -68,7 +68,64 @@ const AddClass = () => {
 
     return (
         <div>
-            
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div className="md:flex gap-8 my-4">
+                    <div className="form-control w-full mb-4">
+                        <label className="label">
+                            <span className="label-text font-semibold">Instructor  Name</span>
+                        </label>
+                        <input type="text" readOnly defaultValue={user?.displayName}
+                            {...register("instructorName")}
+                            className="input input-bordered w-full " />
+                    </div>
+                    <div className="form-control w-full mb-4">
+                        <label className="label">
+                            <span className="label-text font-semibold">Instructor  Email</span>
+                        </label>
+                        <input type="email" readOnly defaultValue={user?.email}
+                            {...register("instructorEmail")}
+                            className="input input-bordered w-full " />
+                    </div>
+                </div>
+
+
+                <div className="md:flex  gap-8 my-4">
+
+                    <div className="form-control mt-4 w-full ">
+                        <label className="label">
+                            <span className="label-text font-semibold">Class Name</span>
+                        </label>
+                        <input type="text" {...register("className", { required: true })} placeholder="Class Name" className="input input-bordered w-full " />
+                    </div>
+                    <div className="form-control w-full my-4">
+                        <label className="label">
+                            <span className="label-text">Class Image</span>
+                        </label>
+                        <input type="file" {...register("image", { required: true })} className="file-input file-input-bordered w-full " />
+                    </div>
+                </div>
+                <div className="md:flex gap-8 my-4">
+                    <div className="form-control w-full mb-4">
+                        <label className="label">
+                            <span className="label-text font-semibold">Available  Seats</span>
+                        </label>
+                        <input type="number" placeholder="Available  Seats"
+                            {...register("availableSeats", { required: true })}
+                            className="input input-bordered w-full " />
+                    </div>
+                    <div className="form-control w-full mb-4">
+                        <label className="label">
+                            <span className="label-text font-semibold">Class  Price</span>
+                        </label>
+                        <input type="number" placeholder="Class  Price"
+                            {...register("classPrice", { required: true })}
+                            className="input input-bordered w-full " />
+                    </div>
+                </div>
+                <div className="w-full mx-auto flex justify-center">
+                    <input className="btn bg-red-600 hover:bg-black border-0 text-white w-full" type="submit" value="Add Class" />
+                </div>
+            </form>
         </div>
     );
 };
