2e92522ec7db39d8d40093b1550934b702b9e590
bossnoyon00 <bossnoyon584@gmail.com>
2023-06-16 00:31:41 +0600

    payment route

--- src/pages/Dashboard/AllUser/hook/usePayment.jsx
+++ src/pages/Dashboard/AllUser/hook/usePayment.jsx
@@ -3,17 +3,7 @@ import { useContext } from 'react';
 import { AuthContext } from '../../../Home/provider/AuthProvider';
 import useAxios from './useAxios';
 const usePayment = () => {
-    const { user, loading } = useContext(AuthContext)
-    const [axiosSecure] = useAxios();
-    const { refetch, data: paymentHistory = [] } = useQuery({
-        queryKey: ['paymentHistory', user?.email],
-        enabled: !loading,
-        queryFn: async () => {
-            const res = await axiosSecure(`/studentsPaymentsHistory?email=${user?.email}`)
-            console.log('res from axios', res)
-            return res.data;
-        },
-    })
+   
 
     return [paymentHistory,  refetch]
 
