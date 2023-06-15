777131ca35d29b284aed6c83de361814ff9edb9b
bossnoyon00 <bossnoyon584@gmail.com>
2023-06-16 00:30:49 +0600

    admin verified

--- src/pages/Dashboard/AllUser/hook/useAddInstructor.jsx
+++ src/pages/Dashboard/AllUser/hook/useAddInstructor.jsx
@@ -1,6 +1,13 @@
 import { useQuery } from "@tanstack/react-query";
 
 const useAddInstructor = () => {
+const {data: instructors = [], isLoading: loading, refetch} = useQuery({
+    queryKey: ['instructors'],
+    queryFn: async() => {
+        const res = await fetch('https://summer-season.vercel.app/instructors');
+        return res.json();
+    }
+})
 
 return [instructors, loading, refetch]
 }
