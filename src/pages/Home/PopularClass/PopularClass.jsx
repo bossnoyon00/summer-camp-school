78552469cd3c6d3122a08c2a4cbfec8cbd53ff75
bossnoyon00 <bossnoyon584@gmail.com>
2023-06-15 17:45:56 +0600

    all user

--- src/pages/Home/PopularClass/PopularClass.jsx
+++ src/pages/Home/PopularClass/PopularClass.jsx
@@ -19,7 +19,13 @@ const PopularClass = () => {
 
     return (
         <div>
-            
+            <h2 className='text-4xl my-8 text-center text-sky-400'>Popular Classes</h2>
+
+            <div className='grid  px-12 md:grid-cols-3 gap-5'>
+                {
+                    popularClass.slice(0, 6).map(pClass => <PopularClassCard pClass={pClass} key={pClass._id}></PopularClassCard>)
+                }
+            </div>
         </div>
     );
 };
